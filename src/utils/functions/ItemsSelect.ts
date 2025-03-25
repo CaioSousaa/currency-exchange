b3733d38c01ae14d95304d0a2069c3c1dfd602d7
CaioSousaa <caiothalles607@gmail.com>
2025-03-24 23:32:31 -0300

    chore: using new currencies

--- src/utils/functions/ItemsSelect.ts
+++ src/utils/functions/ItemsSelect.ts
@@ -3,7 +3,7 @@ export function ItemSelect(items: { key: string; value: string }[] | null) {
     return;
   }
 
-  const indices = [6, 19, 27, 58, 159, 122, 104, 184, 166]; //specific indexes
+  const indices = [6, 19, 27, 47, 151, 59, 80, 74, 123]; //specific indexes
   const itemSelect = indices.map((index) => items[index]).filter(Boolean);
 
   return itemSelect;
